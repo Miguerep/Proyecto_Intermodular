423841fa74df7d5831fc553e6011d7b6370213b9
mmvES <miguerepvfr@gmail.com>
2025-04-08 11:06:36 +0200

    Metodos adicionales

--- ReservasCoches/src/main/java/equipoverde/reservascoches/controlador/ControladorInicioSesion.java
+++ ReservasCoches/src/main/java/equipoverde/reservascoches/controlador/ControladorInicioSesion.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import modelos.UsuariosAplicacion;
 
 /**
  *
@@ -16,7 +17,7 @@ import javax.persistence.TypedQuery;
  */
 public class ControladorInicioSesion {
 
-    String nombreBDO = "AD_Ejemplo1.odb";
+    String nombreBDO = "Usuarios.odb";
     String rutaBase = "$objectdb/db/";
     String rutaBDO = rutaBase + nombreBDO;
     String jpql;
@@ -28,5 +29,32 @@ public class ControladorInicioSesion {
         EntityManager em;
         em = emf.createEntityManager();
     }
+    public void CerrarConexion(EntityManager em, EntityManagerFactory emf){
+            em.close();
+            emf.close();
+    }
+    
+    public void CrearUsuarios(EntityManager em) {
+        try {
+            // inicio transacción bloque hacer datos persistentes -> uno por cada commit()
+            em.getTransaction().begin();
+            UsuariosAplicacion u1 = new UsuariosAplicacion("Juan", "1234");
+            UsuariosAplicacion u2 = new UsuariosAplicacion("Admin", "1234");
+            UsuariosAplicacion u3 = new UsuariosAplicacion("Pedro", "1234");
+       
+            // indicación de hacer persistentes los objetos vehículo
+            em.persist(u1);
+            em.persist(u2);
+            em.persist(u3);
+
+            // confirmacion de la persistencia
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            System.err.println("\tError al ejecutar la transacción.");
+        }
+
+    }
+    
 
 }
