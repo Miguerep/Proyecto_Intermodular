96d95aca35309cb19f692f022f9261bbe00b2cd0
mmvES <miguerepvfr@gmail.com>
2025-04-08 10:14:04 +0200

    Creación clase UsuarioAplicacion

---
+++ ReservasCoches/src/main/java/modelos/UsuariosAplicacion.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+/**
+ *
+ * @author Portatil
+ */
+
+    @Entity
+public class UsuariosAplicacion implements Serializable {
+    @Id
+    private String nombre;
+    private String contraseña;
+
+    public UsuariosAplicacion() {
+    }
+
+
+    public UsuariosAplicacion(String nombre, String contraseña) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
+
+ 
