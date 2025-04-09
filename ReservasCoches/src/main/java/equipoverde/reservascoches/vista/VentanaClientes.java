db95f6384959f45445fd31cad60238bec45759bb
victorgp12 <victorvp200412@gmail.com>
2025-04-09 14:22:03 +0200

    Update VentanaClientes.java

--- ReservasCoches/src/main/java/equipoverde/reservascoches/vista/VentanaClientes.java
+++ ReservasCoches/src/main/java/equipoverde/reservascoches/vista/VentanaClientes.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package VentanaClientes;
+package equipoverde.reservascoches.vista;
 
-import equipoverde.reservascoches.controlador.ControladorInicioSesion.java
 import javax.swing.table.DefaultTableModel;
 
 
