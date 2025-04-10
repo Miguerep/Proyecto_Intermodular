374c12b7665eb9f207d5ec410e4a30d309252da7
mmvES <miguerepvfr@gmail.com>
2025-04-10 08:37:06 +0200

    Metodo obtener todo

--- ReservasCoches/src/main/java/controladores/ControladorReservas.java
+++ ReservasCoches/src/main/java/controladores/ControladorReservas.java
@@ -4,10 +4,13 @@
  */
 package controladores;
 
+import java.lang.System.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
 
 /**
  *
@@ -60,8 +63,91 @@ public class ControladorReservas {
         }
     }
 
-    public Object[][] convertirAMatrizObject() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+ public Object[][] obtenerTodo() {
+
+        Object[][] tabla = null;
+
+        ResultSet rs;
+
+        String sql = "SELECT * FROM deportistas";
+
+        int numRegistros;
+
+        int contador = 0;
+
+        String nomYApe, deporte;
+
+        int añoNac;
+
+        float altura;
+
+
+
+        try {
+
+            rs = sentencia.executeQuery(sql);
+
+            //vamos al último registro y obtenemos su posición para saber cuantos registros hay
+
+            rs.last();
+
+            numRegistros = rs.getRow();
+
+
+
+            // damos tamaño a la matriz de objetos para almacenar lo leído de la BD
+
+            System.out.println("registros obtenidos:" + numRegistros);
+
+            tabla = new Object[numRegistros][4];
+
+
+
+            // recolocamos el puntero para recorrer el resultado
+
+            rs.beforeFirst();
+
+            System.out.println("Lista de deportistas:");
+
+            while (rs.next()) {
+
+                // preparamos los datos
+
+                nomYApe = rs.getString("nomYApe");
+
+                deporte = rs.getString("deporte");
+
+                añoNac = rs.getInt("añoNac");
+
+                altura = rs.getFloat("altura");
+
+                //mostramos los datos por el terminal para hacer seguimiento de la ejecución
+
+                System.out.println("Deportista: " + nomYApe + "\t Deporte: " + deporte + "\t Año nacimiento: " + añoNac + "\t Altura: " + altura);
+
+                // guardamos los datos en la tabla a devolver
+
+                tabla[contador][0] = nomYApe;
+
+                tabla[contador][1] = deporte;
+
+                tabla[contador][2] = añoNac;
+
+                tabla[contador][3] = altura;
+
+                // avanzamos posición en el array
+
+                contador++;
+
+            }
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(ControladorReservas.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return tabla;
+
     }
 
     public void borrarPorNombre(String nomArchivo) {
