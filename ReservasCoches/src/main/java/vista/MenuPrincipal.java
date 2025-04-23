b99d1f9f68c404699ce9e02692de54375f4af53c
JaimePonceSaez <jaimeponcesaez@gmail.com>
2025-04-23 13:15:21 +0200

    ventana

--- ReservasCoches/src/main/java/vista/MenuPrincipal.java
+++ ReservasCoches/src/main/java/vista/MenuPrincipal.java
@@ -115,15 +115,14 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-//                Servicios vent;     
-//        vent = new Servicios();
-//            vent.setVisible(true);
-//            this.dispose();
+                Servicios vent;     
+        vent = new Servicios();
+            vent.setVisible(true);
+            this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
